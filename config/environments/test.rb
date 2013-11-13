17cea3873b5bc27ea2bb6c8fec8de3fa86e655d4
revivre <lilli.life@gmail.com>
2013-11-14 01:30:45 +0900

    Finish user sign up

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # To speed up for bcrypt --added
+  ActiveModel::SecurePassword.min_cost = true
+
 end
