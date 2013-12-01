63e9686779741c48c28308e823f40cf2765f98fb
revivre <lilli.life@gmail.com>
2013-12-01 14:10:42 +0900

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -81,22 +81,43 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end        
       end
 
-      describe "in the Microposts controller" do
-
+      describe "in the Relationships controller" do
         describe "submitting to the create action" do
-          before { post microposts_path }
+          before { post relationships_path }
           specify { expect(response).to redirect_to(signin_path) }
         end
 
         describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          before { delete relationship_path(1) }
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
-
     end
+
+     describe "in the Microposts controller" do
+
+       describe "submitting to the create action" do
+         before { post microposts_path }
+         specify { expect(response).to redirect_to(signin_path) }
+       end
+
+       describe "submitting to the destroy action" do
+         before { delete micropost_path(FactoryGirl.create(:micropost)) }
+         specify { expect(response).to redirect_to(signin_path) }
+       end
+     end
 
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
